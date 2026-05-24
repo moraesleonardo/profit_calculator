d7adba4ab042f9859567cea299cd7281fd110c40
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-05-24 09:26:28 -0300

    first commit

--- profit_calculator.go
+++ profit_calculator.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+
 )
 
 func main(){
-	fmt.Println("Hello World")
+
+var revenue float64
+var expenses float64
+var tax_rate float64
+
+fmt.Print("Revenue: ")
+fmt.Scan(&revenue)
+fmt.Print("Expenses: ")
+fmt.Scan(&expenses)
+fmt.Print("Tax Rate: ")
+fmt.Scan(&tax_rate)
+
+earning_before_tax := revenue - expenses
+earnings_after_tax := earning_before_tax - (earning_before_tax * (tax_rate / 100))
+ratio := earning_before_tax / earnings_after_tax
+
+fmt.Println(earning_before_tax)
+fmt.Println(earnings_after_tax)
+fmt.Printf("%.2f\n", ratio)
+
 }
