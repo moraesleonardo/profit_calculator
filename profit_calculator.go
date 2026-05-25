8d5591326f6d15b36811285c2067bb330b8b4901
Leonardo Moraes <leonardo.moraes1@yahoo.com>
2026-05-25 17:15:25 -0300

    Function use

--- profit_calculator.go
+++ profit_calculator.go
@@ -7,22 +7,37 @@ import (
 
 func main(){
 
-var revenue float64
-var expenses float64
-var tax_rate float64
+//var revenue float64
+//var expenses float64
+//var tax_rate float64
 
-fmt.Print("Revenue: ")
-fmt.Scan(&revenue)
-fmt.Print("Expenses: ")
-fmt.Scan(&expenses)
-fmt.Print("Tax Rate: ")
-fmt.Scan(&tax_rate)
+//fmt.Print("Revenue: ")
+//fmt.Scan(&revenue)
+//fmt.Print("Expenses: ")
+//fmt.Scan(&expenses)
+//fmt.Print("Tax Rate: ")
+//fmt.Scan(&tax_rate)
 
-earning_before_tax := revenue - expenses
-earnings_after_tax := earning_before_tax + (earning_before_tax * (tax_rate / 100))
-ratio := earning_before_tax / earnings_after_tax
+revenue := getUserInput("Revenue: ") 
+expenses := getUserInput("Expenses: ") 
+tax_rate := getUserInput("Tax Rate:: ") 
 
+earning_before_tax, earnings_after_tax, ratio := calculateFinancials(revenue, expenses, tax_rate)
 fmt.Println(earning_before_tax)
 fmt.Println(earnings_after_tax)
 fmt.Printf("Ratio is: %.2f\n", ratio)
+}
+
+func calculateFinancials(revenue, expenses, tax_rate float64)(float64, float64, float64){
+	earning_before_tax := revenue - expenses
+	earnings_after_tax := earning_before_tax * (1 - tax_rate / 100)
+	ratio := earning_before_tax / earnings_after_tax
+	return earning_before_tax, earnings_after_tax, ratio
+}
+
+func getUserInput(infoText string) float64 {
+	var userInput float64
+	fmt.Print(infoText)
+	fmt.Scan(&userInput)
+	return userInput
 }
